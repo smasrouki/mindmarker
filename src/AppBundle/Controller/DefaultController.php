e3915838e6a896969823cbe269d5d82c48b38444
Selim Masrouki <smasrouki@gmail.com>
2016-07-30 10:07:22 +0000

    homepage

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,9 +14,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->redirect($this->generateUrl('subject_index'));
     }
 }
